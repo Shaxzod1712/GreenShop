fb44da13843725ec385e206391eb3441475ca1f7
Shaxzod1712 <shukurullaev1712@gmail.com>
2022-02-24 11:31:31 +0300

    test

--- src/Component/Body/index.jsx
+++ src/Component/Body/index.jsx
@@ -9,12 +9,37 @@ import m6 from '../../assets/imgs/m6.png'
 import m7 from '../../assets/imgs/m7.png'
 import m8 from '../../assets/imgs/m8.png'
 import m9 from '../../assets/imgs/m9.png'
+import m12 from '../../assets/imgs/super-sale-flower.png'
+import m13 from '../../assets/imgs/Super-Sale-Top.png'
 
 
 class Body extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            count: 0,
+        }
+    }
     render(){
+        const decriment =()=>{
+            if(this.state.count > 0){
+            this.setState({
+                    count : this.state.count - 1
+
+                })
+            }
+        }
+        const incriment = ()=>{
+            if(this.state.count < 10){
+            this.setState({
+                count : this.state.count + 1  
+            })
+        }
+        }
         return(
             <div className="container_body">
+                <div className="container-body">
+
                 <div className="bodywrapper1">
                     <div className="categories">
                         <h4>Categories</h4>
@@ -62,6 +87,13 @@ class Body extends React.Component{
                             </div>
                         </div>
                     </div>
+                    <div className="salephoto">
+                        <div className="salephotobackgraund">
+
+                        </div>
+                        <img src={m13} alt="" className="salephoto2"/>
+                        <img src={m12} alt="" className="salephoto1"/>
+                    </div>
                 </div>
 
                 <div className="bodywrapper2">
@@ -79,56 +111,152 @@ class Body extends React.Component{
                         <div className="body__">
                             <div className="bodyfoto1">
                                 <img src={m1} alt="" />
-                                <h5>Barberton Daisy</h5>
-                                <p>$199.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Barberton Daisy</h5>
+                                        <p>$119.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
                             <div className="bodyfoto1">
                                 <img src={m2} alt="" />
-                                <h5>Angel Wing Begonia</h5>
-                                <p>$169.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Angel Wing Begonia</h5>
+                                        <p>$169.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
                             <div className="bodyfoto1">
                                 <img src={m3} alt="" />
-                                <h5>African Violet</h5>
-                                <p>$199.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>African Violet</h5>
+                                        <p>$199.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
+
+                            
                         </div>
 
                         <div className="body__">
                             <div className="bodyfoto1">
                                 <img src={m4} alt="" />
-                                <h5>Beach Spider Lily</h5>
-                                <p>$129.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Beach Spider Lily</h5>
+                                        <p>$129.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
                             <div className="bodyfoto1">
                                 <img src={m5} alt="" />
-                                <h5>Blushing Bromeliad</h5>
-                                <p>$139.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Blushing Bromeliad</h5>
+                                        <p>$139.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
                             <div className="bodyfoto1">
                                 <img src={m6} alt="" />
-                                <h5>Aluminum Plant</h5>
-                                <p>$179.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Aluminum Plant</h5>
+                                        <p>$179.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
+
+                            
                         </div>
 
                         <div className="body__">
                             <div className="bodyfoto1">
                                 <img src={m7} alt="" />
-                                <h5>Bird's Nest Fern</h5>
-                                <p>$39.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Bird's Nest Fern</h5>
+                                        <p>$99.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
                             <div className="bodyfoto1">
                                 <img src={m8} alt="" />
-                                <h5>Broadleaf Lady Palm</h5>
-                                <p>$59.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Broadleaf Lady Palm</h5>
+                                        <p>$59.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
                             <div className="bodyfoto1">
                                 <img src={m9} alt="" />
-                                <h5>Chinese Evergreen</h5>
-                                <p>$99.00</p>
+                                <div className="bodyfoto1_">
+                                    <div className="bodyfoto1_right">
+                                        <h5>Chinese Evergreen</h5>
+                                        <p>$39.00</p>
+                                    </div>
+                                    <div className="bodyfoto1_left">
+                                        <h1 className="decriment" onClick={decriment}>-</h1>
+                                        <h1 className="count">{this.state.count}</h1>
+                                        <h1 className="incriment" onClick={incriment}>+</h1>
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
+                    </div>
+                </div>
+                </div>
+                <div className="bodyend">
+                    <div className="bodyend1">
+                    <h2>1</h2>
+                    <h2>2</h2>
+                    <h2>3</h2>
+                    <h2>4</h2>
+                    <h2>{'>'}</h2>
                     </div>
                 </div>
             </div>
